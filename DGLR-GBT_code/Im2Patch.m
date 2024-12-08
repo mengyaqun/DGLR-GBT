17ec070d377488be50d0b83dd155db5ace8bba9c
mengyaqun <127077289+mengyaqun@users.noreply.github.com>
2024-12-08 20:11:54 +0800

    Update Im2Patch.m

--- DGLR-GBT_code/Im2Patch.m
+++ DGLR-GBT_code/Im2Patch.m
@@ -3,7 +3,7 @@
 % input:    im: target image                                   **
 %           par: parameters                                    **
 % output:   X: patch array                                     **
-% Im2Patch函数的目的是将图像转换为 patches 数组。它接受一个目标图像im和一个参数par作为输入，并返回一个变量X，即 patches 数组。
+
 
 function  X  =  Im2Patch( im, par )
 f       =   par.win;
@@ -19,9 +19,3 @@ for i  = 1:f
         X(k,:) =  blk(:)';
     end
 end
-%初始化 patches 数组X，其尺寸为(f*f, L)，其中f是参数par中的窗口大小，L是 patches 数组的长度。
-%初始化计数器k，用于记录 patches 数组中的当前 patch。
-%使用嵌套循环遍历图像的每个像素。外循环控制行，内循环控制列。在每次迭代中，将当前像素值存储在临时变量blk中。
-%然后将blk的转置（即按列存储）复制到 patches 数组X的第k行。
-%在循环结束后，返回 patches 数组X。
-%输出的变量X表示从输入图像im中提取的 patches 数组。
